ffa347dfd6876c0260832d74e4ab175424db34c2
Alan Kovac <alan.kovac@email.cz>
2022-06-28 12:18:16 +0200

    documented & resolved #10

--- BE/test/setup.ts
+++ BE/test/setup.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server"
 
+/**
+ * This serves as setup for all tests, we need to create DB server which will live in memory for testing purposes, othervise we would CRUD in production DB ❗❗❗
+ * @author Alan Kovac
+ */
+
 let mongo: MongoMemoryServer;
 
 // Spool up the server
@@ -13,7 +18,9 @@ beforeAll(async () => {
 //mongo & connection kill
 afterAll(async () => {
 
-    //db clean up
+    /**
+     * DB clean up - after all test we will clean DB so we can have clean DB which was not mutated
+     */
     const collections = await mongoose.connection.db.collections();
     for (let collection of collections) {
         await collection.deleteMany({})
